de711a78f00ce026b022d08543fc582ba07f706b
Igor Tuag <igortuag@gmail.com>
2020-05-21 20:34:49 -0300

    Create API REST

---
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -0,0 +1,6 @@
+const BillingCycle = require('./BillingCycle')
+
+BillingCycle.methods(['get', 'post', 'put', 'delete'])
+BillingCycle.updateOptions({new: true, runValidators: true})
+
+module.exports = BillingCycle
