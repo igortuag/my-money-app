47310cbafa3371a43964595c924c6e343b984974
Igor Tuag <igortuag@gmail.com>
2020-05-22 07:30:28 -0300

    Getting Billing Cycle Summary route

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -3,6 +3,16 @@ const BillingCycle = require('./BillingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 BillingCycle.route('summary', (req, res, next) => {
     BillingCycle.aggregate([{ 
         $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}} 
