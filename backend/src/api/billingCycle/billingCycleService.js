b92771dd2800827f426610bb7a100b06a222cfca
Igor Tuag <igortuag@gmail.com>
2020-05-22 07:18:54 -0300

    Getting quantity of records

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -3,4 +3,14 @@ const BillingCycle = require('./BillingCycle')
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 BillingCycle.updateOptions({new: true, runValidators: true})
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if(error) {
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json({value})
+        }
+    })
+})
+
 module.exports = BillingCycle
