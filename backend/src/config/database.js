dc45da1e87be637c9701a8b25e32c648acb33148
Igor Tuag <igortuag@gmail.com>
2020-05-22 07:22:25 -0300

    Att mongoose at Mongo 3.6

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost/mymoney')
+module.exports = mongoose.connect('mongodb://localhost/mymoney', { useNewUrlParser: true })
 
 mongoose.Error.messages.general.require = "The '{PATH}' attribute is mandatory."
 mongoose.Error.messages.Number.min = "The '{VALUE}' entered is less than the minimum threshold of '{MIN}'."
