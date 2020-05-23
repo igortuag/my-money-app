361ed3600e61224fca0ca86f9a7afea903145760
Igor Tuag <igortuag@gmail.com>
2020-05-23 11:44:45 -0300

    Footer component

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2017
+            <a href="https://www.linkedin.com/in/igortuag/" target='_blank'> Tuag In</a>
+        </strong>
+    </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
+import Footer from '../common/template/footer'
 
 export default props => (
     <div className='wrapper'>
         <Header />
         <SideBar />
+        <div className='content-wrapper'>
+            Content
+        </div>
+        <Footer />
     </div>
 )
