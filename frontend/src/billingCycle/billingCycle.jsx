59c4ffaee126085db0b11c732c461b48765a7f14
Igor Tuag <igortuag@gmail.com>
2020-05-23 13:13:20 -0300

    NavBar with React-Router + Link + IndexRoute

---
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <div>
+        <h1>Billing Cycle</h1>
+    </div>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -10,7 +10,7 @@ export default props => (
         <Header />
         <SideBar />
         <div className='content-wrapper'>
-            Content
+            {props.children}
         </div>
         <Footer />
     </div>
