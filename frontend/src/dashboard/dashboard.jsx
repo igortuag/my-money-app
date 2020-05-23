d1dd796c081a403d489bca73de96ae2adec2d945
Igor Tuag <igortuag@gmail.com>
2020-05-23 17:18:03 -0300

    Contents Components

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <Content title='Dashboard' small='Versão 1.0' />
+                <Content>
+                    Dashboard
+                </Content>
+            </div>
+        )
+    }
+}
+
+export default Dashboard
