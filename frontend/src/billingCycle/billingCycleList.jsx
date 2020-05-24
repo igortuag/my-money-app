45a70d1af1c2b1627af066e210da88edbdde518b
Igor Tuag <igortuag@gmail.com>
2020-05-24 20:39:37 -0300

    Integrate billingCycleList with redux

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import  { connect } from 'react-redux'
+import { getList } from './billingCycleActions'
 
 class BillingCycleList extends Component {
 
+    componentWillMount() {
+        this.props.getList()
+    }
+    
     render() {
+        console.log(this.props.list)
         return (
             <div>
                 <table className='table'>
@@ -22,4 +30,6 @@ class BillingCycleList extends Component {
     }
 }
 
-export default BillingCycleList
+const mapStateToProps = state => ({list: state.billingCycle.list})
+const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
--- frontend/src/billingCycle/billingCycleReducer.js
+++ frontend/src/billingCycle/billingCycleReducer.js
@@ -2,7 +2,7 @@ const INITIAL_STATE = {list: []}
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case 'BILLING_CYCLE_FETCHED':
+        case 'BILLING_CYCLES_FETCHED':
             return { ...state, list: action.payload.data }
         default:
             return state
