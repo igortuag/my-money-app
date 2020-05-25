a3c4f5d8b64d775087c146f834cdd6c53dfedfb8
Igor Tuag <igortuag@gmail.com>
2020-05-25 08:04:16 -0300

    Component custom field

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
+import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends Component {
     render() {
@@ -8,9 +9,12 @@ class BillingCycleForm extends Component {
             <div>
                 <form role='form' onSubmit={handleSubmit}>
                     <div className='box-body'>
-                        <Field name='name' component='input' />
-                        <Field name='month' component='input' />
-                        <Field name='year' component='input' />
+                        <Field name='name' component={labelAndInput} 
+                            label='Name' cols='12 4' placeholder='Enter the name' />
+                        <Field name='month' component={labelAndInput} type='number'
+                            label='Month' cols='12 4' placeholder='Enter the month' />
+                        <Field name='year' component={labelAndInput} type='number'
+                            label='Year' cols='12 4' placeholder='Enter the year' />
                     </div>
                     <div className='box-footer'>
                         <button type='submit' className='btn btn-primary'>Submit</button>
