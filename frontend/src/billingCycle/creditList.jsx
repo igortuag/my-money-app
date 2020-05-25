055fb65dafe47079397be730d817eb43300fa227
Igor Tuag <igortuag@gmail.com>
2020-05-25 20:17:52 -0300

    Remove credits

--- frontend/src/billingCycle/creditList.jsx
+++ frontend/src/billingCycle/creditList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { Field, arrayInsert } from 'redux-form'
+import { Field, arrayInsert, arrayRemove } from 'redux-form'
 import Grid from '../common/layout/grid'
 import Input from '../common/form/input'
 
@@ -10,6 +10,12 @@ class CreditList extends Component {
     add(index, item = {}) {
         if(!this.props.readOnly) {
             this.props.arrayInsert('billingCycleForm', 'credits', index, item)
+        }
+    }
+
+    remove(index) {
+        if(!this.props.readOnly && this.props.list.length > 1) {
+            this.props.arrayRemove('billingCycleForm', 'credits', index)
         }
     }
 
@@ -29,6 +35,10 @@ class CreditList extends Component {
                     <button type='button' className='btn btn-warning'
                         onClick={() => this.add(index + 1, item)}>
                         <i className='fa fa-clone' />
+                    </button>
+                    <button type='button' className='btn btn-danger'
+                        onClick={() => this.remove(index)}>
+                        <i className='fa fa-trash' />
                     </button>
                 </td>
             </tr>
@@ -58,5 +68,5 @@ class CreditList extends Component {
     }
 }
 
-const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert }, dispatch)
+const mapDispatchToProps = dispatch => bindActionCreators({ arrayInsert, arrayRemove }, dispatch)
 export default connect(null, mapDispatchToProps)(CreditList)
