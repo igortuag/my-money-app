2b55ff6f40e50b4b434410ea9b34ecf3a6678f2b
Igor Tuag <igortuag@gmail.com>
2020-05-26 20:51:07 -0300

    fixed auth.js

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -8,13 +8,13 @@ module.exports = (req, res, next) => {
         const token = req.body.token || req.query.token || req.headers['authorization']
     
         if (!token) {
-            return res.status(403).send({ erros: ['No token provided.'] })
+            return res.status(403).send({ errors: ['No token provided.'] })
         }
 
         jwt.verify(token, env.authSecret, function(err, decoded) {
             if(err) {
                 return res.status(403).send({
-                    erros: ['Failed to authenticate token.']
+                       errors: ['Failed to authenticate token.']
                 })
             } else {
                 // req.decoded = decoded
