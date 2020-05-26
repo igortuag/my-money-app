396f7f5d5830da028e98878e161b3b3a084a009a
Igor Tuag <igortuag@gmail.com>
2020-05-26 20:00:33 -0300

    Validate token rule

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -12,3 +12,30 @@ const sendErrorsFromDB = (res, dbErrors) => {
     _.forIn(dbErrors.errors, error => errors.push(error.message))
     return res.status(400).json({ errors })
 }
+
+const login = ( req, res, next) => {
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user && bcrypt.compareSync(password, user.password)) {
+            const token = jwt.sign(user, env. authSecret, {
+                expiresIn: "1 day"
+            })
+            const { name, email } = user
+            res.json({ name, email, token })
+        } else {
+            return res.status(400).send({ errors: ['User/Password invalid']})
+        }
+    })
+}
+
+const validateToken = (req, res, next) => {
+    const token = req.body.token || ''
+
+    jwt.verify(token, env.authSecret, function (err, decoded) {
+        return res.status(200).send({ valid: !err })
+    })
+}
