95a140a5a80cb3459c6da44061827b34ed08d1f7
Igor Tuag <igortuag@gmail.com>
2020-05-26 20:30:08 -0300

    Config new auth routes

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,27 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
+    
+    /*
+    * Protected Routes JWT Token
+    */
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
 
-    // Definir URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
+    protectedApi.use(auth)
 
-    // Rotas de Ciclo de Pagamento
     const BillingCycle = require('../api/billingCycle/billingCycleService')
     BillingCycle.register(router, '/billingCycles')
+
+    /*
+    * Open Routes
+    */
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
