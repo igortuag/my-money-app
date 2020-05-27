0bb1fe681163e3b2dbb0dfd1b5814c3c435be912
Igor Tuag <igortuag@gmail.com>
2020-05-27 19:53:01 -0300

    Fixed messages error

--- frontend/src/auth/auth.jsx
+++ frontend/src/auth/auth.jsx
@@ -8,7 +8,7 @@ import { login, signup } from './authActions'
 import Row from '../common/layout/row'
 import Grid from '../common/layout/grid'
 import If from '../common/operator/if'
-import Messages from '../common/msg/mesages'
+import Messages from '../common/msg/messages'
 import Input from '../common/form/inputAuth'
 
 class Auth extends Component {
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
-import Messages from '../common/msg/mensages'
+import Messages from '../common/msg/messages'
 
 export default props => (
     <div className='wrapper'>
